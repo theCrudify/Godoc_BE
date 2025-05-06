7964266ad953765c20d8610907a2aa68f8342894
kupattahugoyang <kupattahugoyang@gmail.com>
2025-05-07 01:56:08 +0700

    feat : adding summary for dashboard

--- src/main-structure/Activity/Email/EmailAuthorization/Email_Approval_AuthDoc.ts
+++ src/main-structure/Activity/Email/EmailAuthorization/Email_Approval_AuthDoc.ts
@@ -55,7 +55,7 @@ const getEmailData = async (authorizationDocId: number, authId: number) => {
             step: 'asc'
           }
         }
-      }
+      } 
     });
 
     if (!authorizationDoc) {
